9d8cd9d647907caac2061c819ff3c7715388a9a6
Yerin Lee <rt8632@naver.com>
2025-08-18 22:21:02 +0900

    initial commit

---
+++ src/main/java/com/shinhan/heybob/HeybobApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.heybob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeybobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeybobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shinhan/heybob/HeybobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan.heybob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeybobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
