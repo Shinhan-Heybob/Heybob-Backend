c915442b903c9bae7864d616ca75857511a0d142
Yerin Lee <rt8632@naver.com>
2025-08-20 23:19:57 +0900

    setting : 메인 서버 및 채팅 서버 레포 병합

---
+++ ChatServer/src/test/java/com/shinhan/heybob/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan.heybob.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
