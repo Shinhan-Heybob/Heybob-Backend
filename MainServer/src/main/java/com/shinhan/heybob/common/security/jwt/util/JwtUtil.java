e97f24b57a76b5d59b0b74adda59a410fa1d343c
marinRim <marinrim23@gmail.com>
2025-08-21 10:56:32 +0900

    feat: token추출, ì생성 구현

--- MainServer/src/main/java/com/shinhan/heybob/common/security/jwt/util/JwtUtil.java
+++ MainServer/src/main/java/com/shinhan/heybob/common/security/jwt/util/JwtUtil.java
@@ -1,5 +1,8 @@
 package com.shinhan.heybob.common.security.jwt.util;
 
+import com.shinhan.heybob.common.security.user.UserPrincipalDetails;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -7,6 +10,7 @@ import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Slf4j
 @Component
@@ -36,5 +40,47 @@ public class JwtUtil {
         return secretKey;
     }
 
+    public String generateAccessToken(UserPrincipalDetails userPrincipalDetails) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + accessTokenValiditySeconds * 1000); // ms로 넣어야 함
+
+        return Jwts.builder()
+                .setSubject(userPrincipalDetails.getUsername())
+                .claim("email", userPrincipalDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String generateRefreshToken(UserPrincipalDetails userPrincipalDetails) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + refreshTokenValiditySeconds * 1000);
+
+        return Jwts.builder()
+                .setSubject(userPrincipalDetails.getUsername())
+                .claim("email", userPrincipalDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String getUsernameFromAccessToken(String token) {
+        return extractUsername(token);
+    }
+
+    public String getUsernameFromRefreshToken(String token) {
+        return extractUsername(token);
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload().getSubject();
+
+    }
 }
 
