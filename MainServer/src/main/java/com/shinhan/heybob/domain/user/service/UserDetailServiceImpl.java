92f6b44d5a28a8e3be3007fce225a26527fe86b0
marinRim <marinrim23@gmail.com>
2025-08-21 12:53:47 +0900

    feat: UserDetailServiceImpl 구현

---
+++ MainServer/src/main/java/com/shinhan/heybob/domain/user/service/UserDetailServiceImpl.java
@@ -0,0 +1,34 @@
+package com.shinhan.heybob.domain.user.service;
+
+import com.shinhan.heybob.common.user.UserPrincipalDetails;
+import com.shinhan.heybob.domain.user.entity.User;
+import com.shinhan.heybob.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        User findUser = findUserByEmailOrThrow(userEmail);
+
+        List<GrantedAuthority> authorities = Collections.singletonList((GrantedAuthority)  () -> "ROLE_USER");
+        return new UserPrincipalDetails(findUser, authorities);
+    }
+
+    private User findUserByEmailOrThrow(String userEmail) {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new Exception());
+    }
+}
