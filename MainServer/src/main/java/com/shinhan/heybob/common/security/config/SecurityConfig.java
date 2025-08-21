b69d511a0ec58894f5d03ab24f6504c08bd16a4b
marinRim <marinrim23@gmail.com>
2025-08-21 10:10:28 +0900

    feat: SecurityConfig 생성

---
+++ MainServer/src/main/java/com/shinhan/heybob/common/security/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.shinhan.heybob.common.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+}
