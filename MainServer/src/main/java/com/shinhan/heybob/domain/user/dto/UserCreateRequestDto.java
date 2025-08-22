73e67127304b27acb30f270ae1c3e36a991ec3f9
marinRim <marinrim23@gmail.com>
2025-08-22 17:48:43 +0900

    fix: NotBlank 안되서 수정

--- MainServer/src/main/java/com/shinhan/heybob/domain/user/dto/UserCreateRequestDto.java
+++ MainServer/src/main/java/com/shinhan/heybob/domain/user/dto/UserCreateRequestDto.java
@@ -1,7 +1,9 @@
 package com.shinhan.heybob.domain.user.dto;
 
 import com.shinhan.heybob.domain.user.entity.User;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -31,7 +33,8 @@ public class UserCreateRequestDto {
     @NotBlank
     private String department;
 
-    @NotBlank(message = "개인정보 수집 및 이용에 동의해야 합니다.")
+    @NotNull(message = "개인정보 수집 및 이용 동의 여부는 필수입니다.")
+    @AssertTrue(message = "개인정보 수집 및 이용에 동의해야 회원가입이 가능합니다.")
     private Boolean agreeTerms;
 
     public User toEntity(UserCreateRequestDto userCreateRequestDto, String encryptedPassword) {
