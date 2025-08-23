4cd199cc2b5c2860a0bb8f009a8bb1ea353e950d
mirim <marinrim23@gmail.com>
2025-08-23 16:54:48 +0900

    feat: Repo 생성

---
+++ MainServer/src/main/java/com/shinhan/heybob/domain/finance/repository/ExternalFinanceUserRepository.java
@@ -0,0 +1,14 @@
+package com.shinhan.heybob.domain.finance.repository;
+
+import com.shinhan.heybob.domain.finance.entity.ExternalFinanceUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ExternalFinanceUserRepository extends JpaRepository<ExternalFinanceUser, Long> {
+
+    Optional<ExternalFinanceUser> findByUserId(String userId);
+
+}
