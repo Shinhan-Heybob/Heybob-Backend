f8d834e028dc9e5eeb4d2b17e02e005c6b6ccb12
mirim <marinrim23@gmail.com>
2025-08-23 16:50:13 +0900

    feat: ExternalFinanceUser 생성

---
+++ MainServer/src/main/java/com/shinhan/heybob/domain/finance/entity/ExternalFinanceUser.java
@@ -0,0 +1,31 @@
+package com.shinhan.heybob.domain.finance.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "external_finance_user",
+    uniqueConstraints = {
+            @UniqueConstraint(name = "uk_ext_userid", columnNames = "userId")
+    })
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExternalFinanceUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;   // User 엔티티의 id와 동일
+
+    @Column(length = 40, nullable = false, unique = true)
+    private String userId;  // 외부 시스템에 넘길 이메일 형식 ex) abcd1234@ssafy.com
+
+    @Column(length = 128, nullable = false)
+    private String userKey; // 금융API로부터 받은 userKey
+
+    public void updateKey(String newKey) {
+        this.userKey = newKey;
+    }
+}
