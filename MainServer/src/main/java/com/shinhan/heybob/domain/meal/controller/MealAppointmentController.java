8401c713fa02d2e895e2896341b8daa4ab2576b9
mirim <marinrim23@gmail.com>
2025-08-24 23:11:04 +0900

    fix: /api 삭제

--- MainServer/src/main/java/com/shinhan/heybob/domain/meal/controller/MealAppointmentController.java
+++ MainServer/src/main/java/com/shinhan/heybob/domain/meal/controller/MealAppointmentController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/meal-appointments")
+@RequestMapping("/meal-appointments")
 @RequiredArgsConstructor
 public class MealAppointmentController {
 
