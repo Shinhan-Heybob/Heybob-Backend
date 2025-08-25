c937956455841ab7076a87d741c13614bf80b218
marinRim <marinrim23@gmail.com>
2025-08-25 13:51:01 +0900

    docs: log 추가

--- MainServer/src/main/java/com/shinhan/heybob/domain/notification/dto/ChatEventMessageDto.java
+++ MainServer/src/main/java/com/shinhan/heybob/domain/notification/dto/ChatEventMessageDto.java
@@ -7,7 +7,7 @@ public record ChatEventMessageDto(
         Long chatRoomId,
         Long settlementId,
         Long initiatorId,
-        String initiatorName,
+        String initiatorName, // "이지민"
         String title,        // "이지민님이 정산하기를 요청했습니다!"
         String ctaLabel      // "30,450원 송금하기"
 ) {}
