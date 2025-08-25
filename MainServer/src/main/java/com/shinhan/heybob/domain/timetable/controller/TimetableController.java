fc02981a087b70a62e6364e01709520f806a5c1f
rt8632 <rt8632@naver.com>
2025-08-25 17:59:27 +0900

    feat : 시간표 삭제 API 구현

--- MainServer/src/main/java/com/shinhan/heybob/domain/timetable/controller/TimetableController.java
+++ MainServer/src/main/java/com/shinhan/heybob/domain/timetable/controller/TimetableController.java
@@ -7,12 +7,7 @@ import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/timetable")
 @RequiredArgsConstructor
@@ -30,5 +25,11 @@ public class TimetableController {
     @GetMapping("/{timeTableId}")
     public ResponseEntity<TimetableGetResponseDto> getTimetable(@PathVariable Long timeTableId) {
         return ResponseEntity.ok(timetableService.getTimeTable(timeTableId));
+    }
+
+    @DeleteMapping("/{timeTableId}")
+    public ResponseEntity<Void> deleteTimetable(@PathVariable Long timeTableId) {
+        timetableService.deleteTimeTable(timeTableId);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
